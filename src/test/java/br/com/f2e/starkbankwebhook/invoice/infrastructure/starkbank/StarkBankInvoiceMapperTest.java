260d5cd8cff7535496ce377e523938609e9be3bb
franklin.barreto <franklin-barreto@hotmail.com>
2026-08-12 21:55:29 -0300

    test: make invoice mapper fixture immutable

--- src/test/java/br/com/f2e/starkbankwebhook/invoice/infrastructure/starkbank/StarkBankInvoiceMapperTest.java
+++ src/test/java/br/com/f2e/starkbankwebhook/invoice/infrastructure/starkbank/StarkBankInvoiceMapperTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 class StarkBankInvoiceMapperTest {
 
-  private StarkBankInvoiceMapper starkBankInvoiceMapper = new StarkBankInvoiceMapper();
+  private final StarkBankInvoiceMapper starkBankInvoiceMapper = new StarkBankInvoiceMapper();
 
   @Test
   void shouldMapInvoiceDraftToStarkBankInvoice() {
